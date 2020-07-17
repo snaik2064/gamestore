a746cc1169bb85ccf961ac7091ad1379eadf5b93
shashank <snaik2064@gmail.com>
2020-07-17 18:45:25 +0530

    Game screen styling complete.

--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -8,6 +8,25 @@ import Text from '../Text';
 
 const GameScreen = ({route, navigation}) => {
   const {game} = route.params;
+
+  const renderStars = () => {
+    let stars = [];
+
+    for (let s = 1; s <= 5; s++) {
+      stars.push(
+        <Icon
+          key={s}
+          name="star"
+          size={16}
+          style={{marginRight: 5}}
+          color={Math.floor(game.rating) >= s ? '#819ee5' : '#434958'}
+        />,
+      );
+    }
+
+    return <Stars>{stars}</Stars>;
+  };
+
   return (
     <GameContainer>
       <StatusBar barStyle="light-content" />
@@ -31,6 +50,36 @@ const GameScreen = ({route, navigation}) => {
           <Icon name="download" size={30} color="#ffffff" />
         </Download>
       </GameInfoContainer>
+      <GameStatusContainer>
+        {renderStars()}
+
+        <Text heavy color="#9a9a9a">
+          {game.rating}
+        </Text>
+        <Text bold color="#9a9a9a">
+          {game.category[0]}
+        </Text>
+        <Text bold color="#9a9a9a">
+          {game.age}
+        </Text>
+        <Text bold color="#9a9a9a">
+          Game of the day
+        </Text>
+      </GameStatusContainer>
+      <ScreenShotsContainer>
+        <ScreenShots horizontal showsHorizontalScrollIndicator={false}>
+          {game.screenshots.map((screenshot, index) => {
+            return (
+              <ScreenShotContainer key={index}>
+                <ScreenShot source={screenshot} />
+              </ScreenShotContainer>
+            );
+          })}
+        </ScreenShots>
+      </ScreenShotsContainer>
+      <Description medium color="#9a9a9a">
+        {game.description}
+      </Description>
     </GameContainer>
   );
 };
@@ -92,4 +141,40 @@ const Download = styled.View`
   border-radius: 20px;
   align-items: center;
   justify-content: center;
+`;
+
+const GameStatusContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0 16px;
+`;
+
+const Stars = styled.View`
+  flex-direction: row;
+`;
+
+const ScreenShotsContainer = styled.View`
+  margin: 8px 0;
+`;
+
+const ScreenShotContainer = styled.View`
+  padding: 16px;
+  shadow-color: #000000;
+  shadow-offset: 1px 1px;
+  shadow-opacity: 0.5;
+  shadow-radius: 5px;
+`;
+
+const ScreenShots = styled.ScrollView``;
+
+const ScreenShot = styled.Image`
+  height: 200px;
+  width: 300px;
+  border-radius: 12px;
+`;
+
+const Description = styled(Text)`
+  position: relative;
+  margin: 0 16px;
+  line-height: 22px;
 `;
--- components/screens/LiveScreen.js
+++ components/screens/LiveScreen.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'react-native';
+
+import styled from 'styled-components';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Text from '../Text';
+import games from '../../constants/gameData';
 
 const LiveScreen = () => {
   return (
-    <View>
-      <Text>LiveScreen</Text>
-    </View>
+    <Container>
+      <StatusBar barStyle="light-content" />
+
+      <Header>
+        <Text large bold>
+          Streaming
+        </Text>
+        <SearchContainer>
+          <Search placeholder="Search....." placeholdertextColor="#838383" />
+          <SearchIcon>
+            <Icon name="search" size={24} color="#838383" />
+          </SearchIcon>
+        </SearchContainer>
+      </Header>
+    </Container>
   );
 };
 
 export default LiveScreen;
 
-const styles = StyleSheet.create({});
+const Container = styled.SafeAreaView`
+  background-color: #343434;
+  flex: 1;
+`;
+
+const Header = styled.View`
+  margin: 32px 16px 0 16px;
+`;
+
+const SearchContainer = styled.View`
+  position: relative;
+  margin: 23px 8px;
+  background-color: #404040;
+  border-radius: 100px;
+  justify-content: center;
+`;
+
+const Search = styled.TextInput`
+  padding: 16px 64px 16px 32px;
+  color: #c6c6c6;
+`;
+
+const SearchIcon = styled.TouchableOpacity`
+  position: absolute;
+  right: 16px;
+`;
